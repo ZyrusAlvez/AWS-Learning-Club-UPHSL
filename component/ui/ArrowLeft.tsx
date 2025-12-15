efec6c73b56f5f6b529d8ecdc981762aaccbec9c
ZyrusAlvez <zyrusalvez13@gmail.com>
2025-12-15 23:54:02 +0800

    style: change shade of yellow

--- component/ui/ArrowLeft.tsx
+++ component/ui/ArrowLeft.tsx
@@ -7,10 +7,10 @@ type Props = {
 const Arrow_Left = ({ onClick } : Props) => {
   return (
     <div
-      className="w-11 h-11 border-2 border-pink rounded-full flex flex-shrink-0 justify-center items-center cursor-pointer group hover:bg-pink active:bg-pink"
+      className="w-11 h-11 border-2 border-[#ffa23f] rounded-full flex flex-shrink-0 justify-center items-center cursor-pointer group hover:bg-[#ffa23f] active:bg-[#ffa23f]"
       onClick={onClick}
     >
-      <FaArrowLeft className="text-pink group-hover:text-white text-base" />
+      <FaArrowLeft className="text-[#ffa23f] group-hover:text-white text-base" />
     </div>
   );
 };
