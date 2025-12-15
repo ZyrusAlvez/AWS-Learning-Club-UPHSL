4ba64bea94c62bee12a2fbabf81addab75c31e62
ZyrusAlvez <zyrusalvez13@gmail.com>
2025-12-16 07:29:15 +0800

    feat: footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
 import "../styles/globals.css";
+import Footer from "@/component/Footer";
+
 export default function RootLayout({children}: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ component/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='text-center border-t-[1.5px] border-[#ffa23f] mx-4 mt-8 py-2 text-white/70'>
+      <h1>© 2025 AWS Cloud Clubs Philippines. All rights reserved.</h1>
+    </div>
+  )
+}
+
+export default Footer
