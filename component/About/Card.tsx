ef5caf610a94228452e2e9e17108d30676985157
ZyrusAlvez <zyrusalvez13@gmail.com>
2026-01-08 01:10:24 +0800

    style: top cards

--- component/About/Card.tsx
+++ component/About/Card.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Top_Card = ({ title, subtitle }: {title: string, subtitle: string}) => {
   return (
-    <div className="border-[#ffa23f] border-2 rounded-2xl px-4 py-6 sm:h-40 text-white flex flex-col justify-center font-inter w-full sm:w-80 max-w-sm">
+    <div className="border-yellow-500 bg-yellow-500/10 border-2 rounded-2xl px-4 py-6 sm:h-40 text-white flex flex-col justify-center font-inter w-full sm:w-80 max-w-sm">
       <h1 className="text-xl sm:text-2xl mb-2 font-bold">{title}</h1>
       <h2 className="text-sm sm:text-base">{subtitle}</h2>
     </div>
